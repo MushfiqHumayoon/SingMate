642ba760503c4317c769fbadbabbc95e922462bb
Mushfiq <mushfiqhumayoon@gmail.com>
2024-03-29 00:27:16 +0400

    Initial Commit

---
+++ SingMate/SingMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingMateApp.swift
+//  SingMate
+//
+//  Created by Mushfiq Humayoon on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SingMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
